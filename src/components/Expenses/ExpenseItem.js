de7152c2144b05672d0b5e692ac54021574acb9d
AlSid604 <alex_sidor97@hotmail.com>
2022-02-14 22:23:09 -0800

    updated with card wrapper

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,8 +1,8 @@
 import ExpenseDate from "./ExpenseDate";
-import Card from "./Card";
+import Card from "../UI/Card";
 import "./ExpenseItem.css";
-
-function ExpenseItem(props) {
+//Inside wrapper.- CARD
+const ExpenseItem = (props) => {
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -10,8 +10,9 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button>Change Title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import ExpenseItem from "./ExpenseItem";
-import Card from "./Card";
+import Card from "../UI/Card";
 import "./Expenses.css";
 
-function Expenses(props) {
+const Expenses = (props) => {
   return (
     <Card className="expenses">
       {props.items.map((item) => (
@@ -10,5 +10,5 @@ function Expenses(props) {
       ))}
     </Card>
   );
-}
+};
 export default Expenses;
